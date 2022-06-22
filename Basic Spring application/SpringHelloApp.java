c451816304af24a0cff20f037958c6bbcdd5b055
palseums <57275214+palseums@users.noreply.github.com>
2022-06-22 13:21:28 -0500

    Basic Spring application template
    Basic Spring application template

---
+++ Basic Spring application/SpringHelloApp.java
@@ -0,0 +1,29 @@
+package com.spring.learning;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringHelloApp {
+
+	public static void main(String[] args) {
+		
+		//load the spring configuration file
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve bean from spring container
+		
+		Coach thecoach = context.getBean("myCoach", Coach.class);
+		
+		// call methods on the bean
+		
+		System.out.println(thecoach.getDailyWorkout());
+		
+		System.out.println(thecoach.getDailyFortune());
+		
+		//close the context
+		
+		context.close();
+
+	}
+
+}
