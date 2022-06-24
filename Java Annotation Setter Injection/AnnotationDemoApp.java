4c32adab11324add724ee54fe753f32103b326dd
palseums <57275214+palseums@users.noreply.github.com>
2022-06-24 17:11:31 -0500

    Java Annotation Setter Injection
    Java Annotation Setter Injection

---
+++ Java Annotation Setter Injection/AnnotationDemoApp.java
@@ -0,0 +1,20 @@
+package com.spring.annotations;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach thecoach = context.getBean("tennisCoach",Coach.class);
+		
+		System.out.println(thecoach.getDailyWorkout());
+		System.out.println(thecoach.getDailyFortune());
+		
+		context.close();
+
+	}
+
+}
